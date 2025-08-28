36f7e435f0c87f4a4bf887bd2f9200380e8bc4b8
Seigo Fukuoka <seigo.fukuoka@every.tv>
2025-08-28 18:45:45 +0900

    アイドルのスケジュールアプリの基本機能を実装

---
+++ hitandblow.js
@@ -0,0 +1,11 @@
+//readlineモジュールをインポート
+const readline = require("readline");
+
+// HitAndBlowクラスを定義、クラスはこれだけ
+class HitAndBlow {
+    constructor() {
+        // 4桁の数値を生成
+        randdomNumber
+    }
+}
+
---
+++ test.js
@@ -0,0 +1,14 @@
+const mapData = [
+            "#########",
+            "#.   o  #",
+            "#   @   #",
+            "# o   . #",
+            "#       #",
+            "#       #",
+            "#########",     
+        ];
+map = mapData.map(row => row.split("")); // 文字列を配列に変換
+console.log(map);
+map.forEach(function (row) => {
+    
+});
